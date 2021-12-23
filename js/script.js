9e92c634bbd1686eff5ad2afcc2ba3d23a6c2e0b
Mary <yunmaryko@gmail.com>
2021-12-23 17:25:28 +0600

    changed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-var year = +prompt('Введите год');
+var askDate = +prompt('Введите год')
 
-if (year % 4 === 0){
-    alert('Это високосный год (366 дней)')
+if (askDate % 100 !== 0 && askDate % 4 === 0){
+    alert('Это високосный год')
+} else if (askDate % 100 === 0 && askDate % 400 === 0){
+    alert('Это високосный год')
 } else {
-    alert('Это не високосный год (365 дней)')
+    alert('Это не високосный год')
 }
